db6f73e04a11918740aff1e3a473184b40e97006
jjuszak <jjuszak@usgs.gov>
2018-03-29 11:56:30 -0600

    WIP

--- javascripts/nbm/widgets/HBPElasticWidget.js
+++ javascripts/nbm/widgets/HBPElasticWidget.js
@@ -5,17 +5,24 @@
  * @param {*} chartData - chart data from the server
  * @constructor
  */
-var HierarchyByPixelWidget = function(chartData) {
+var HBPElasticWidget = function(chartData) {
+    var that = this;
+    this.config = chartData;
     var hierarchy;
-    this.getHtml = function() {
+
+    this.getHtml = function () {
+        return getHtmlFromJsRenderTemplate('#pixelHierarchyPlaceholder', {id:that.bap.id});
+    };
+
+    this.getInitializedHtml = function() {
         var html = '';
-        hierarchy = chartData.hierarchy;
         for(var i = 0; i < hierarchy.length; i++) {
             var level = hierarchy[i];
             //data is returned from lowest hierarchy level (ecological system) to highest (class) while bioscape lists them in opposite order
-            level.isOn = ((hierarchy.length - 1) - i) == chartData.layerIndex;
-            level.isLast = i == hierarchy.length - 1;
-            if(i == 0) {
+            level.isOn = ((hierarchy.length - 1) - i) === that.config.layerIndex;
+            level.isLast = i === hierarchy.length - 1;
+            if(i === 0) {
+                level.url = that.elasticUrl;
                 html = getHtmlFromJsRenderTemplate('#identifyUnitTemplate', level);
             } else {
                 level.additionalHtml = html;
@@ -76,7 +83,62 @@ var HierarchyByPixelWidget = function(chartData) {
         }
     };
 
-    this.initializeWidget = function() {
+    this.massageData = function (obj) {
+        var prefixes = [
+            "ecosystem_",
+            "group_",
+            "macrogroup_",
+            "division_",
+            "formation_",
+            "subclass_",
+            "class_"
+        ];
 
+        var levels = [];
+        that.config.layerIndex = that.bap.actionRef.updatedParams.layerLevel;
+
+        for (var i = 0; i < prefixes.length; i++) {
+            var prefix = prefixes[i];
+            levels.push({
+                id: "hierarchyLevel"+i,
+                code: obj[prefix + "code"],
+                description: obj[prefix + "description"],
+                title: obj[prefix + "title"],
+                type: obj[prefix + "type"]
+            });
+        }
+
+        hierarchy = levels;
+    };
+
+    this.initializeWidget = function() {
+        var that = this;
+
+        var elasticQuery = {
+            "query" : {
+                "term" : {}
+            }
+        };
+
+        elasticQuery["query"]["term"][this.config.elasticTerm] = that.bap.actionRef.result.geojson.properties[that.bap.actionRef.lookupProperty];
+
+        that.elasticUrl = this.config.elasticUrl + encodeURI(JSON.stringify(elasticQuery));
+        $.getJSON(that.elasticUrl, function (data) {
+            var hit = data["success"]["hits"]["hits"][0]["_source"]["properties"];
+            that.massageData(hit);
+
+            var jsonButton = getHtmlFromJsRenderTemplate('#rawJsonButtonTemplate', {url: that.elasticUrl});
+            var initializedPackage = that.getInitializedHtml();
+            $("#hierarchyChart"+that.bap.id).html(jsonButton + initializedPackage);
+
+            that.bindClicks();
+        });
+    };
+
+    this.bindClicks = function () {
+        $(".hierarchyExpanders").on('click', function() {
+            var id = $(this).data('section');
+            toggleContainer(id);
+        });
     };
 };
--- javascripts/nbm/widgets/WidgetHelper.js
+++ javascripts/nbm/widgets/WidgetHelper.js
@@ -7,23 +7,25 @@ var WidgetHelper = function () {
 WidgetHelper.prototype.getWidget = function (config,bap) {
     if (config.type == "hierarchyByPixel") {
         return new HierarchyByPixelWidget(config);
-    } else if (config.type == "hierarchyTable") {
+    } else if (config.type === "hierarchyTable") {
         return new HierarchyTableWidget(config);
-    } else if (config.type == "eco") {
+    } else if (config.type === "eco") {
         return new EcosystemProtectionWidget(config);
-    } else if (config.type == "species") {
+    } else if (config.type === "species") {
         return new SpeciesProtectionWidget(config);
-    } else if (config.type == "richness") {
+    } else if (config.type === "richness") {
         return new SpeciesRichnessWidget(config);
-    } else if (config.type == "dynamicMatrix") {
+    } else if (config.type === "dynamicMatrix") {
         return new CnrMatrixWidget(config);
     } else if (config.type == "boxAndWhisker") {
         return new BoxAndWhiskerWidget(config,bap);
     } else if (config.type == "rasterQuery") {
         return new BarChartWidget(config);
-    } else if (config.type == "vectorQuery") {
+    } else if (config.type === "vectorQuery") {
         return new BarChartWidget(config);
-    } else if (config.type == "nfhp") {
+    } else if (config.type === "nfhp") {
         return new NFHPTestWidget(config);
+    } else if (config.type === "hierarchyByPixelElastic") {
+        return new HBPElasticWidget(config);
     }
 };
