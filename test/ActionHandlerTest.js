5320a334226e9577999aadeef38136bc963d0e38
jjuszak <jjuszak@usgs.gov>
2018-10-09 12:44:21 -0600

    Remove obsolete test files

--- test/ActionHandlerTest.js
+++
@@ -1,9 +0,0 @@
-describe('ActionHandler.js', function() {
-    var testActionHandler = new ActionHandler({}, undefined);
-
-    describe('created', function() {
-        it('ActionHandler object successfully created', function() {
-            assert.isDefined (testActionHandler, "action handler should not be undefined");
-        });
-    });
-});
